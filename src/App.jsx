90c72e6e055f4f182719e3f58095f6e401120535
Rohit <heartstealarrohitthakur@gmail.com>
2024-09-22 13:24:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+
+const app=()=>{
+  return <div>
+    <Home />
+    <About />
+    <Nav />
+  </div>
+}
+export default app;
+
+
+//rafce
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// we can use export default only once in a single file 
+// we can import exported thing with any name but remember we have to follow naming convention  
+
+// we can use export  const many times  in a single file 
+// we can import exported thing within curly braces remember we have to give same name with we are exporting things   
+
+// only return thing will shows on brower  and all type of logics  are written in body of an fnc  and showing part is in return 
+// jsx = js with help of html 
+// we can use html tag in jsx like <h1>Hello</h1>
+
+// feature of return we can not return more than one and  code does'nt run after return 
+ 
